eb7402ca254132aef818d18feb860c75c09109db
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-02-27 22:47:44 -0300

    Add books controller

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,4 +1,24 @@
 package guru.springframework.spring5webapp.controllers;
 
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        // Here I can return any atribute I add to the model that
+        // will be returned to the view
+        return "books/list";
+    }
 }
